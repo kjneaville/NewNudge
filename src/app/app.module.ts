10f1b1dbd98b6c571f9ad449e3e83bf894a958f0
Matthew McKenna <mnmckenn@uw.edu>
2017-05-04 21:25:13 -0700

    login skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,64 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import {
+    BrowserModule
+} from '@angular/platform-browser';
+import {
+    ErrorHandler,
+    NgModule
+} from '@angular/core';
+import {
+    IonicApp,
+    IonicErrorHandler,
+    IonicModule
+} from 'ionic-angular';
+import {
+    SplashScreen
+} from '@ionic-native/splash-screen';
+import {
+    StatusBar
+} from '@ionic-native/status-bar';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { LevelsPage } from '../pages/levels/levels';
-import { ListPage } from '../pages/list/list';
-import { NudgePage } from '../pages/nudge/nudge';
-
+import {
+    MyApp
+} from './app.component';
+import {
+    HomePage
+} from '../pages/home/home';
+import {
+    LevelsPage
+} from '../pages/levels/levels';
+import {
+    ListPage
+} from '../pages/list/list';
+import {
+    NudgePage
+} from '../pages/nudge/nudge';
 @NgModule({
-  declarations: [
+    declarations: [
     MyApp,
     HomePage,
     LevelsPage,
     ListPage,
     NudgePage
   ],
-  imports: [
+    imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
-  bootstrap: [IonicApp],
-  entryComponents: [
+    bootstrap: [IonicApp],
+    entryComponents: [
     MyApp,
     HomePage,
     LevelsPage,
     ListPage,
     NudgePage
   ],
-  providers: [
+    providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+        {
+            provide: ErrorHandler,
+            useClass: IonicErrorHandler
+        }
   ]
 })
 export class AppModule {}
---
+++ src/pages/Login/login.ts
@@ -0,0 +1,53 @@
+import firebase from 'https://www.gstatic.com/firebasejs/3.9.0/firebase.js'
+// Initialize Firebase
+// TODO: Replace with your project's customized code snippet
+var config = {
+    apiKey: "<API_KEY>",
+    authDomain: "<PROJECT_ID>.firebaseapp.com",
+    databaseURL: "https://<DATABASE_NAME>.firebaseio.com",
+    storageBucket: "<BUCKET>.appspot.com",
+};
+firebase.initializeApp(config);
+
+var loginGoogle = function () {
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+};
+
+var loginFacebook = function () {
+    var provider = new firebase.auth.FacebookAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+}
