c2c24dcf6bf2e6bb109f197df06d7f0a5c634fff
Matthew McKenna <mnmckenn@uw.edu>
2017-05-05 10:33:33 -0700

    added Facebook login + account-settings skeleton

--- src/pages/authentication/authentication.ts
+++ src/pages/authentication/authentication.ts
@@ -25,6 +25,7 @@ var config = {
 
 firebase.initializeApp(config);
 
+
 /**
  * Generated class for the Authentication page.
  *
@@ -44,9 +45,31 @@ export class Authentication {
         console.log('ionViewDidLoad Authentication');
     }
 
-    login() {
-        console.log('here')
+    loginGoogle() {
         var provider = new firebase.auth.GoogleAuthProvider();
+
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            var token = result.credential.accessToken;
+            // The signed-in user info.
+            var user = result.user;
+            // ...
+        }).catch((error: any) => {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // ...
+        });
+        this.navCtrl.push(HomePage); // Log in and go to home page
+    }
+
+    loginFacebook() {
+        var provider = new firebase.auth.FacebookAuthProvider();
 
         firebase.auth().signInWithPopup(provider).then(function (result) {
 
